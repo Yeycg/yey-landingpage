8b33ff873b441edf569dc8905f289db420c56573
Yey Castano <yeygonzalez@gmai.com>
2023-05-18 20:55:14 -0400

    ajustes de menu mobile

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 /*.**********menu*********. */
-((d) =>{
-    const $btnMenu = d.querySelector(".menu-btn"),
-     $menu = d.querySelector(".menu");
+const btnMenuMobile = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menuMobile');
+const Close = document.querySelector('.none')
 
-    $btnMenu.addEventListener ("click",(e) => {
-        $btnMenu.firstElementChild.classList.toggle("none");
-        $btnMenu.lastElementChild.classList.toggle("none");
-        $menu.classList.toggle("is-active");
+btnMenuMobile.addEventListener('click', toggleMobilemenu) ;
 
-    });
+function toggleMobilemenu() {
+    menuMobile.classList.toggle ('inactive');
 
-    d.addEventListener("click", (e)=>{
-        if(!e.target.matches(".menu a")) return false;
-        $btnMenu.firstElementChild.classList.remove("none");
-        $btnMenu.lastElementChild.classList.add("none");
-        $menu.classList.remove("is-active");
-
-    })
-})(document);
+}
